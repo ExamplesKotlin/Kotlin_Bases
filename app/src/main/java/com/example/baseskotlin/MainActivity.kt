37c1afa71e28e66c854dec1e5a1d7e450e848a46
Henry AT <yiperu@gmail.com>
2019-10-08 23:53:03 -0300

    convertToBase64Str

--- app/src/main/java/com/example/baseskotlin/MainActivity.kt
+++ app/src/main/java/com/example/baseskotlin/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.baseskotlin
 
+import android.annotation.TargetApi
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 import kotlin.experimental.and
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +15,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        convertIntToByteArray()
+//        convertIntToByteArray()
+        convertToBase64Str()
+//        convertHexStringToByteArray()
     }
 
     fun convertIntToByteArray() {
@@ -19,8 +26,9 @@ class MainActivity : AppCompatActivity() {
 //        val bytes: ByteArray = getByteArrayFromInt(number)
         val bytes: Array<Byte> = getByteArrayFromInt(number)
 
-        println("IntValue($number) => BinaryString(" +
-                "${bytes.joinToString { x -> "${getByteBinaryStr(x)}" }})"
+        println(
+            "IntValue($number) => BinaryString(" +
+                    "${bytes.joinToString { x -> "${getByteBinaryStr(x)}" }})"
         )
     }
 
@@ -53,4 +61,78 @@ class MainActivity : AppCompatActivity() {
 
         return result
     }
+
+    // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun convertToBase64Str() {
+
+        val text = "abcde"
+
+        println("text: $text")
+        println()
+//        println("From API: ${fromAPI(text)}")
+//        println("From Manual: ${manual(text)}")
+        Log.e("TAG", "From API: ${fromAPI(text)}")
+        Log.e("TAG", "From Manual: ${manual(text)}")
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun fromAPI(text: String): String {
+
+        var result = ""
+        val bytes = text.toByteArray()
+        result = String(Base64.getEncoder().encode(bytes))
+
+        return result
+    }
+
+    private fun manual(text: String): String {
+
+        var result = StringBuilder()
+        val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+        val bytes: Array<Byte> = text.toByteArray().toTypedArray()
+
+        var nHighBits = 0
+        var nLowBits = 0
+
+        var code = 0
+        var prevItRemainingBits = 0
+
+        for (byte in bytes) {
+            nHighBits = 6 - nLowBits
+            nLowBits = 8 - nHighBits
+
+            code = byte.toInt() shl (8 - nHighBits)
+            code = (prevItRemainingBits shl nHighBits) or code
+
+            result.append(alphabet[code])
+
+            if (nLowBits == 6) {
+
+                result.append(alphabet[prevItRemainingBits])
+
+                // reset
+                prevItRemainingBits = 0
+                nLowBits = 0
+
+            }
+        }
+
+        if (prevItRemainingBits != 0) {
+            val lastCode = prevItRemainingBits shl (6 - nLowBits)
+            result.append(alphabet[lastCode])
+        }
+
+        var padding = 3 - bytes.size % 3
+        if (padding < 3) {
+            while (padding-- > 0) result.append("=")
+        }
+
+        return result.toString()
+    }
+
+
 }
